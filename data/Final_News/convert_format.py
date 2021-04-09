758b9476140160510e319aa243f26699b9cd4408
difu0317 <fud13qsinghua@gmail.com>
2021-04-09 11:06:51 +0800

    data format preprocess

---
+++ data/Final_News/convert_format.py
@@ -0,0 +1,12 @@
+import csv
+
+filename = 'walmart_news_CNN'
+
+
+with open(filename +'_utf8.csv', 'a',encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    with open(filename+'.csv', newline='') as csvfile2:
+        spamreader = csv.reader(csvfile2, delimiter=',')
+        for row in spamreader:
+            if len(row) > 0:
+                writer.writerow([row[0][2:-1],row[1]])
