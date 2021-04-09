65d78875557643683e15c70e5a80b72d5b06576e
difu0317 <fud13qsinghua@gmail.com>
2021-04-09 21:50:20 +0800

    update processing code

--- data/News Triple/triple_dict.py
+++ data/News Triple/triple_dict.py
@@ -1,29 +1,54 @@
 import csv
 import re
+from string import digits
 filename = 'walmart_news_CNN'
 
 
-def extract_triple(str):
+
+
+def clean_triple(str):
+	return re.sub(':','',str).strip()
+
+def extract_triple(list):
+	str = ''
+	for element in list:
+		str += element
 	confidence = float(str[:4])
 	#tuples = str[6:-2]
 	#tuples.split
-	triples = re.findall(r'[(](.*?)[)]', str)[0].split(';')
-	out_list = [confidence]
-	for entity in triples:
-		out_list.append(entity.strip())
-	return out_list
+	triples = re.findall(r'[(](.*?)[)]', str)
+	out_list = []
+	for triple in triples:
+		triple_list = []
+		for str in triple.split(';'):
+			#clean all the number
+			new_str = str.translate(str.maketrans('', '', digits))
+			new_str = re.sub('-|=|"|%','',new_str).strip()
+			#clean data
+			if new_str[:2] == 'L:':
+				triple_list.append(new_str[2:])
+			else:
+				triple_list.append(new_str)
+		if len(triple_list) > 2:
+			#print(triple_list[2].strip())
+			if triple_list[2][:2] != 'T:' and len(clean_triple(triple_list[2])) > 2 :
+				out_list = [confidence]
+				for entity in triple_list:
+					out_list.append(clean_triple(entity))
+				return out_list[:4]
 
-def select_triple(list_tuple):
+#print(extract_triple(['0.93 (The best sales; to shop; 2021-04-06T15:12:57Z)']))
+
+
+def select_triple(list_tuple, thres = 0.8):
 	select_tuple = None
 	best_score = 0
 	for tuples in list_tuple:
-		if tuples[0] > 0.9 and tuples[0] > best_score:
+		if tuples[0] > thres and tuples[0] > best_score:
 			select_tuple = tuples
 			best_score = tuples[0]
 	return select_tuple
 
-
-#print(extract_tuple('1.00 (Amazon; announces; $ 15 minimum wage)'),len(extract_tuple('1.00 (Amazon; announces; $ 15 minimum wage)')))
 
 with open(filename + '_processed.csv', 'a') as csvfile:
     #writer.writerow('news','date','confidence','agent','predicate','object')
@@ -36,8 +61,9 @@ with open(filename + '_processed.csv', 'a') as csvfile:
                 date = row[1]
                 #print(news,date)
                 triple_list = []
-            elif len(row) == 1:
-                triple_list.append(extract_triple(row[0]))
+            elif len(row) > 0:
+                if extract_triple(row) is not None:
+                    triple_list.append(extract_triple(row))
             elif len(row) == 0:
                 selected_triple = select_triple(triple_list)
                 if selected_triple is not None:
@@ -46,4 +72,3 @@ with open(filename + '_processed.csv', 'a') as csvfile:
                         write_row.append(item)
                     writer.writerow(write_row)
                 
-
