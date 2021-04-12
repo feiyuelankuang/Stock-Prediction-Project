76a001fdb6a224cd8e17b4a73d89972ebe5a3607
difu0317 <fud13qsinghua@gmail.com>
2021-04-12 20:36:16 +0800

    fix encoding

--- data-downloader/get_news_vec.py
+++ data-downloader/get_news_vec.py
@@ -8,9 +8,9 @@ import pickle
 model = KeyedVectors.load_word2vec_format('../data/GoogleNews-vectors-negative300.bin', binary=True)
 
 # Loop through folder
-for file in os.listdir('../data/Final_News/'):
+for file in os.listdir('../data/News Triple/'):
     if file.endswith('.csv'):
-        news_df = pd.read_csv('../data/Final_News/' + file, header=None)
+        news_df = pd.read_csv('../data/News Triple/' + file, header=None,encoding='cp1252')
         news_df.rename(columns={0: "content", 1: "time"}, inplace=True)
 
         filename = file.split('.')[0]
