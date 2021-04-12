7c3caef77a57bd6dc9e8a93e7def0855bd5f7f0a
kelkoa <>
2021-04-12 13:08:12 +0800

    Change.ipynb files to .py

---
+++ data-downloader/get_news_vec.py
@@ -0,0 +1,36 @@
+from gensim.models import KeyedVectors
+import pandas as pd
+import os
+import string
+import pickle
+
+# Load vectors directly from the file
+model = KeyedVectors.load_word2vec_format('../data/GoogleNews-vectors-negative300.bin', binary=True)
+
+# Loop through folder
+for file in os.listdir('../data/Final_News/'):
+    if file.endswith('.csv'):
+        news_df = pd.read_csv('../data/Final_News/' + file, header=None)
+        news_df.rename(columns={0: "content", 1: "time"}, inplace=True)
+
+        filename = file.split('.')[0]
+        vec_df = pd.DataFrame()
+
+        for index, row in news_df.iterrows():
+            txt = row.content
+
+            # Remove the first 'b'
+            txt = txt[1:]
+
+            # Remove punctuation
+            txt = txt.translate(str.maketrans('', '', string.punctuation))
+
+            # Remove exception terms
+            resultwords  = [word for word in txt.split() if word in model]
+            txt = ' '.join(resultwords)
+
+            # Add to DataFrame
+            vec_df = vec_df.append({"vec": [model[x] for x in txt.split(' ') if x is not ''], "time": row.time}, ignore_index=True)
+
+        # vec_df.to_csv('../data/News_Vectors/' + file, index=False)
+        vec_df.to_pickle('../data/News_Vectors/' + filename + '.pkl')
---
+++ data-downloader/get_stock_timeseries.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import datetime
+import yfinance as yf
+
+ts_folder = '../data/timeseries_data/'
+
+start_date = datetime.datetime(2010, 1, 2)
+end_date = datetime.datetime(2021, 4, 2)
+
+tickers_str = 'AAPL MSFT SSNLF BA GOOG WMT' # Note: SSNLF not available
+
+stock_data = yf.download(tickers_str, start=start_date, end=end_date, threads=False)
+
+# Save data
+APPL_data = stock_data.xs('AAPL', axis=1, level=1, drop_level=False)
+APPL_data.to_csv(ts_folder + 'AAPL.csv', header=False)
+
+BA_data = stock_data.xs('BA', axis=1, level=1, drop_level=False)
+BA_data.to_csv(ts_folder + 'BA.csv', header=False)
+
+GOOG_data = stock_data.xs('GOOG', axis=1, level=1, drop_level=False)
+GOOG_data.to_csv(ts_folder + 'GOOG.csv', header=False)
+
+MSFT_data = stock_data.xs('MSFT', axis=1, level=1, drop_level=False)
+MSFT_data.to_csv(ts_folder + 'MSFT.csv', header=False)
+
+WMT_data = stock_data.xs('WMT', axis=1, level=1, drop_level=False)
+WMT_data.to_csv(ts_folder + 'WMT.csv', header=False)
